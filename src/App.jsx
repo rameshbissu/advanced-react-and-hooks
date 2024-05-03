5e84977e2c3a500103e21376674fafd6af135a88
rameshbissu <rameshbissu646@gmail.com>
2024-05-03 21:19:33 +0530

    useEffect multiple effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics";
+import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects";
 // import Final from "./tutorial/02-useEffect/final/01-code-example";
 function App() {
   return (
