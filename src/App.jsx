ab36b81ceea6354904b159a431e3de8641806b3c
rameshbissu <rameshbissu646@gmail.com>
2024-05-03 21:14:45 +0530

    useEffect Fundamentals

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/02-useEffect/starter/01-code-example";
+import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics";
 // import Final from "./tutorial/02-useEffect/final/01-code-example";
 function App() {
   return (
