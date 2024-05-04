b664e86cca20d1c1516948994fdfb8eac14b5e8b
rameshbissu <rameshbissu646@gmail.com>
2024-05-04 13:51:36 +0530

    useEffect fetch challenge - complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects";
+import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
 // import Final from "./tutorial/02-useEffect/final/01-code-example";
 function App() {
   return (
