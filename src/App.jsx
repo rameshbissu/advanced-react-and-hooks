ba5b0da6ecff33afe45a25d77ef65f7aa3e5139d
rameshbissu <rameshbissu646@gmail.com>
2024-05-05 17:39:32 +0530

    Toggle Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples";
+import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
 // import Final from "./tutorial/02-useEffect/final/01-code-example";
 function App() {
   return (
