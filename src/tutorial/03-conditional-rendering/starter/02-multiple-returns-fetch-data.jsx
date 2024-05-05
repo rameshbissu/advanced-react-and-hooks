e06977cb33f8cdb589cc52aad10cb7462d9de307
rameshbissu <rameshbissu646@gmail.com>
2024-05-05 16:37:15 +0530

    order matters

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const url = "https://api.github.com/users/QuincyLarsons";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -33,16 +33,17 @@ const MultipleReturnsFetchData = () => {
     return <h2>There was an error.</h2>;
   }
 
+  const { avatar_url, name, company, bio } = user;
   return (
     <div>
       <img
         style={{ width: "150px", borderRadius: "25px" }}
-        src={user.avatar_url}
-        alt={user.name}
+        src={avatar_url}
+        alt={name}
       />
-      <h2>{user.name}</h2>
-      <h4>works at {user.company}</h4>
-      <p>{user.bio}</p>
+      <h2>{name}</h2>
+      <h4>works at {company}</h4>
+      <p>{bio}</p>
     </div>
   );
 };
