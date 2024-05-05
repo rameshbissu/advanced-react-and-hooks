2fddaf68d4fc204de19669ff74650ebe37d3ba33
rameshbissu <rameshbissu646@gmail.com>
2024-05-05 16:56:50 +0530

    short circuit - Overview

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/03-hooks-rule";
+import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
 // import Final from "./tutorial/02-useEffect/final/01-code-example";
 function App() {
   return (
