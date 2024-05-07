04af0c394942f2a5062d23480d542a671c9e1feb
rameshbissu <rameshbissu646@gmail.com>
2024-05-07 17:22:22 +0530

    Project Structure - Named Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
-// import Final from "./tutorial/02-useEffect/final/01-code-example";
+import Navbar from "./tutorial/04-project-structure/starter/Navbar";
+
 function App() {
   return (
     <div className="container">
-      <Starter />
-      {/* <Final/> */}
+      <Navbar />
     </div>
   );
 }
