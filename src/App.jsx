709d68c1d7155098b6c38e2749d837e828b6d1ee
rameshbissu <rameshbissu646@gmail.com>
2024-05-07 11:46:21 +0530

    useEffect Cleanup Function - setup/Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 // import Final from "./tutorial/02-useEffect/final/01-code-example";
 function App() {
   return (
--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [value, setValue] = useState(false);
+
+  const toggleHandle = () => {
+    setValue(!value);
+  };
+
+  return (
+    <>
+      {value ? (
+        <div>
+          <h2>This is Main</h2>
+          <button className="btn" onClick={toggleHandle}>
+            Click me
+          </button>
+        </div>
+      ) : (
+        <AnotherComponent click={toggleHandle} />
+      )}
+    </>
+  );
+};
+
+const AnotherComponent = ({ click }) => {
+  useEffect(() => {
+    console.log("Another component useEffect");
+  }, []);
+  return (
+    <>
+      <h2>The Another Component</h2>
+      <button className="btn" onClick={click}>
+        Click me
+      </button>
+    </>
+  );
 };
 
 export default CleanupFunction;
