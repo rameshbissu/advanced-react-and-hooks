ebc2e77d3bf57bd3f9468fb168500f0537e40979
rameshbissu <rameshbissu646@gmail.com>
2024-05-14 18:05:07 +0530

    Custom Hooks - Generic Fetch

--- src/tutorial/08-custom-hooks/starter/02-fetch-data.jsx
+++ src/tutorial/08-custom-hooks/starter/02-fetch-data.jsx
@@ -1,9 +1,9 @@
-import useFetchPerson from "./useFetchPerson";
+import useFetch from "./useFetch";
 
 const url = "https://api.github.com/users/QuincyLarson";
 
 const FetchData = () => {
-  const { isLoading, isError, user } = useFetchPerson(url);
+  const { isLoading, isError, data: user } = useFetch(url);
   // order matters
   // don't place user JSX before loading or error
 
