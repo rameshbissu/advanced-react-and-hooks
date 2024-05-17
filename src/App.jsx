782bf96c912abc5e70412185b72024bbffbee796
rameshbissu <rameshbissu646@gmail.com>
2024-05-17 14:25:31 +0530

    useReducer - Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Final from "./tutorial/09-context-api/final/context/Navbar";
-import Starter from "./tutorial/09-context-api/starter/Navbar";
+import Starter from "./tutorial/10-useReducer/starter/01-useReducer";
 
 function App() {
   return (
     <>
-      <Final />
+      {/* <Final /> */}
       <Starter />
     </>
   );
---
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { data } from "../../../data";
+const ReducerBasics = () => {
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearList = () => {
+    setPeople([]);
+  };
+  const resetList = () => {
+    setPeople(data);
+  };
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      {people.length > 0 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={() => clearList()}
+        >
+          clear
+        </button>
+      ) : (
+        <button type="button" className="btn" onClick={() => resetList()}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ReducerBasics;
