405176eb107df4c38a16dbe031a267d3d93fbd72
rameshbissu <rameshbissu646@gmail.com>
2024-05-17 14:39:52 +0530

    useReducer setup

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,21 +1,30 @@
-import React from "react";
-import { data } from "../../../data";
-const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+import { useState, useReducer } from "react";
+import { data, people } from "../../../data";
 
+const defaultState = {
+  people: data,
+};
+
+const reducer = () => {};
+
+const ReducerBasics = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  // const [people, setPeople] = useState(data);
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
   const clearList = () => {
-    setPeople([]);
+    // setPeople([]);
   };
   const resetList = () => {
-    setPeople(data);
+    // setPeople(data);
   };
+  console.log(state);
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -24,7 +33,7 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length > 0 ? (
+      {state.people.length > 0 ? (
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
