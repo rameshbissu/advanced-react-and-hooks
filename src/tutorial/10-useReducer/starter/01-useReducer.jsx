adcc40ba83b698b79f2f718f2a0a1f9567a7bd9c
rameshbissu <rameshbissu646@gmail.com>
2024-05-17 15:27:42 +0530

    Import/Export

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,11 @@
 import { useReducer } from "react";
 import { data } from "../../../data";
+import { CLEAR_LIST, REMOVE_ITEM, RESET_LIST } from "./actions";
+import reducer from "./reducer";
 
 const defaultState = {
   people: data,
   isLoading: false,
-};
-
-const CLEAR_LIST = "CLEAR_LIST";
-const RESET_LIST = "RESET_LIST";
-const REMOVE_ITEM = "REMOVE_ITEM";
-
-const reducer = (state, action) => {
-  if (action.type === CLEAR_LIST) {
-    return { ...state, people: [] };
-  }
-  if (action.type === RESET_LIST) {
-    return { ...state, people: data };
-  }
-  if (action.type === REMOVE_ITEM) {
-    const { id } = action.payload;
-    let newPeople = state.people.filter((person) => person.id !== id);
-
-    return { ...state, people: newPeople };
-  }
-  // return state;
-  throw new Error(`No Matching "${action.type}" - action type`);
 };
 
 const ReducerBasics = () => {
@@ -39,6 +20,7 @@ const ReducerBasics = () => {
   const resetList = () => {
     dispatch({ type: RESET_LIST });
   };
+
   return (
     <div>
       {state.people.map((person) => {
