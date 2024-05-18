6503d2089ed74875f273af3d1b430f816b16f22a
rameshbissu <rameshbissu646@gmail.com>
2024-05-18 08:03:53 +0530

    Lower State - Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Final from "./tutorial/11-performance/final/01-lower-state/Counter";
-import Starter from "./tutorial/11-performance/starter/01-lower-state";
+import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge";
 
 function App() {
   return (
