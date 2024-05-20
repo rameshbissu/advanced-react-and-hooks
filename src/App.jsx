d4dba677fb9c0b923ace2c37b339bef791e15a4a
rameshbissu <rameshbissu646@gmail.com>
2024-05-20 16:51:00 +0530

    useTransition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Final from "./tutorial/11-performance/final/03-hooks";
-import Starter from "./tutorial/11-performance/starter/03-hooks";
+import Starter from "./tutorial/11-performance/starter/04-react-18";
 
 function App() {
   return (
